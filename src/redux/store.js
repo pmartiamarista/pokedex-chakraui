e7f1d2ef97c80597b36e62bafae3e5e218209df6
Pedro Marti <pedro@cratebind.com>
2021-07-12 22:11:42 -0400

    fix redux middleware

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,14 +2,10 @@ import { createStore, applyMiddleware, compose } from "redux";
 import thunkMiddleware from "redux-thunk";
 import rootReducer from "./reducers";
 
-let middleware = [];
+let middleware = [thunkMiddleware];
 
 if (process.env.NODE_ENV === "development") {
-  middleware = [
-    ...middleware,
-    thunkMiddleware,
-    require("redux-logger").default,
-  ];
+  middleware = [...middleware, require("redux-logger").default];
 }
 
 const store = createStore(rootReducer, compose(applyMiddleware(...middleware)));
